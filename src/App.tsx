c427d59affb1146cc651a5861dc84c2b8da75224
ndsanja <ndsanja@gmail.com>
2021-06-20 23:04:37 +0700

    01-handling-state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import  "./App.css"
+import React, {useState} from 'react'
+
+interface IState {
+  students : {
+    name: string,
+    age: number,
+    url: string,
+    note?: string
+  }[]
+}
 
 export default function App() {
+const [students, setStudents] = useState<IState["students"]>([])
+
   return (
-    <div>
-      Hello typescript
+    <div className="App">
+      <h1>Student List</h1>
     </div>
   )
 }
